9895fe860befc23084fea7cc0ddbbf5f556cb85b
Tim Henkelmann <ga68vah@mytum.de>
2017-03-13 17:55:29 +0100

    - removed artists.json output and package.json - added .gitignore file and artists_example.json - updated server.js according to tutor input:     - removed server     - removed unnecessary comments     - removed email from request

--- scrapeIDArtists/server.js
+++ scrapeIDArtists/server.js
@@ -1,86 +1,58 @@
 const http = require('http');
 var request = require("request");
 var fs = require('fs');
-var async = require('async');
 
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-
-  var obj = {
+var obj = {
     table: []
-  };
+};
 
-  // obj.table.push({id: 1, type:"person", name:"Lukas Music Star", country: "LT"});
-  // var json = JSON.stringify(obj);
-  // fs.writeFile('artistYYYY.json', json, 'utf8');
+var date = 1620;
 
-  // var start = new Date("1620-11-20"); //yyyy-mm-dd
-  // var end = new Date("1719-04-01"); //yyyy-mm-dd
-  var date = 1620
+setInterval(function () {
+    // once all requests for the year 1620 until 1820 are executed, convert the outputobject into json and write it to file
+    if (date == 1821) {
+        json2 = JSON.stringify(obj); //convert it back to json
+        fs.writeFile('artistYYYY.json', json2, 'utf8', function writeFileCallback(err, data) {
+            process.exit();
+        }); // write it back
+    }
 
-       setInterval(function(){
+    var url = "http://musicbrainz.org/ws/2/artist/?query=begin:" + date + "&fmt=json";
 
-            if(date == 1821) {
-              json2 = JSON.stringify(obj); //convert it back to json
-              fs.writeFile('artistYYYY.json', json2, 'utf8', function writeFileCallback(err, data) {
-                  process.exit();
-              }); // write it back
+    console.log("requesting: " + url);
+
+    request({
+        url: url,
+        headers: {
+            'User-Agent': 'ClassicalMusic/1.5.0 '
+        },
+        json: true
+    }, function (error, response, body) {
+
+        var json = body;
+        var counter = json.count;
+        console.log("number of artists born in " + date + ": " + counter);
+
+        if (counter > 0) {
+
+            // iterate through all entries and push them to the array obj.table
+            for (var z = 0; z < counter; z++) {
+                artist = json.artists[z];
+
+                obj.table.push({
+                    id: artist.id,
+                    type: artist.type,
+                    name: artist.name,
+                    country: artist.country,
+                    date: date
+                });
             }
+        }
+        date = date + 1;
 
-            // var mm = ((start.getMonth()+1)>=10)?(start.getMonth()+1):'0'+(start.getMonth()+1);
-            // var dd = ((start.getDate())>=10)? (start.getDate()) : '0' + (start.getDate());
-            // var yyyy = start.getFullYear();
-            // var date = yyyy+"-"+mm+"-"+dd; //yyyy-mm-dd
-
-            
-
-            var url = "http://musicbrainz.org/ws/2/artist/?query=begin:"+date+"&fmt=json";
-
-            console.log(url);
-
-            console.log('request started!');
-            
-            request({
-                url: url,
-                headers: {
-                'User-Agent': 'ClassicalMusic/1.5.0 ( laucius@gmail.com )'
-                },
-                json: true
-            }, function (error, response, body) {
-
-                    var json = body;
-                    var counter = json.count;
-                    console.log(counter);
-
-                    if (counter > 0) {
-          
-                      // obj = JSON.parse(data); //now it an object
-                        for (var z = 0; z < counter; z++) {
-                          artist = json.artists[z];
-                          
-                          obj.table.push({id: artist.id, type:artist.type, name:artist.name, country:artist.country, date:date});
-                      }  
-                   } 
-
-                   date = date + 1;
-                   
-                      
-            });
-   
-          
-        }, 3000)
+    });
 
 
+}, 3000)
 
-})
 
-        
