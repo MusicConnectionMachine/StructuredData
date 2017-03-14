7d6f8acf8fe128503a2efb506d19d2f9ab50faf3
Tim Henkelmann <ga68vah@mytum.de>
2017-03-14 12:04:25 +0100

    - applied changes to server.js:     - removed server     - removed email

--- scrapeWorks/server.js
+++ scrapeWorks/server.js
@@ -1,78 +1,58 @@
-const http = require('http');
 var request = require("request");
 var fs = require('fs');
-var async = require('async');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+var elemNum = 0;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-
-  var elemNum = 0;
-
-  var newObj = {
+var newObj = {
     table: []
-  };
+};
 
-  fs.readFile('IDartistsOther.json', 'utf8', function readFileCallback(err, data){
-      if (err){
-          console.log(err);
-      } else {
-      
-      obj = JSON.parse(data); //now it an object
-      arrayofObj = obj.table;
+fs.readFile('IDartistsOther.json', 'utf8', function readFileCallback(err, data) {
+    if (err) {
+        console.log(err);
+    } else {
 
-      var finalArray = arrayofObj.map(function (artistai) {
-        return artistai.id;
-      });
+        obj = JSON.parse(data); //now it an object
+        arrayofObj = obj.table;
 
-       setInterval(function(){      
+        var finalArray = arrayofObj.map(function (artistai) {
+            return artistai.id;
+        });
 
-            var url = "http://musicbrainz.org/ws/2/work/?query=arid:"+finalArray[elemNum]+"&fmt=json";
+        setInterval(function () {
+
+            var url = "http://musicbrainz.org/ws/2/work/?query=arid:" + finalArray[elemNum] + "&fmt=json";
 
             if (elemNum == finalArray.length) {
-               json2 = JSON.stringify(newObj); //convert it back to json
-               fs.writeFile('worksOthers.json', json2, 'utf8', function writeFileCallback(err, data) {
-                  process.exit();
-               }); // write it back
-               
+                json2 = JSON.stringify(newObj); //convert it back to json
+                fs.writeFile('worksOthers.json', json2, 'utf8', function writeFileCallback(err, data) {
+                    process.exit();
+                }); // write it back
+
             }
 
             console.log(url);
 
             console.log('request started!');
-            
+
             request({
                 url: url,
                 headers: {
-                'User-Agent': 'ClassicApp/1.4.0 ( tumis@gmail.com )'
+                    'User-Agent': 'ClassicApp/1.4.0 '
                 },
                 json: true
             }, function (error, response, body) {
 
-                    var json = body;
-                    newObj.table.push({artistId: finalArray[elemNum], count: json.count, works: json.works});
-                    elemNum = elemNum + 1;
-                      
+                var json = body;
+                newObj.table.push({artistId: finalArray[elemNum], count: json.count, works: json.works});
+                elemNum = elemNum + 1;
+
             });
 
-           
 
-            
-          
         }, 2000)
 
-        }});
+    }
+});
 
 
-
-})
-
-        
