77381a105c27aaa8af4ad3e09d6241653270dfb0
Angelin Rashmi Antony Rajan <Rashmi@217.215.eduroam.dynamic.rbg.tum.de>
2017-03-15 16:12:47 +0100

    Moved database connection parameters to config file

--- Scripts_to_load_tables/load_entities.js
+++ Scripts_to_load_tables/load_entities.js
@@ -1,38 +1,39 @@
 ////// THIS SCRIPT LOADS THE INPUT JSON FILE INTO ENTITIES TABLE.THIS TABLE IS EXPOSED TO GROUP 2 THORUGH REST API'S //////////////////////////
 var fs = require("fs");
 var pg = require("pg");
-var content = fs.readFileSync("dbpedia_composer.json"); //296
+var content = fs.readFileSync("dbpedia_composer.json");
+var config = require(__dirname + '/cfg/config.template.js');
 var jsonContent = JSON.parse(content);
-var cn = {
-    host: 'localhost',
-    port: 5432,
-    database: 'postgres',
-    user: 'postgres',
-    password: '1234',
-};
+var dbConnParams = config.database;
 
-i=0;
-console.log(jsonContent.length);
-pg.connect(cn, function(err, client, done) {       
-	if (err) {
-           console.log(err);
+function constructConnStr(config) {
+    var connString = "postgres://" + config.username + ":" + config.password +
+        "@" + config.hostname + ":" + config.port + "/" + config.database;
+    console.log("connection string ", connString);
+    return connString;
+}
+i = 0;
+pg.connect(constructConnStr(config), function(err, client, done) {
+    if (err) {
+        console.log(err);
+    }
+    while (i < jsonContent.length) {
+        var work = [],
+            release = [],
+            pseudonym = [];
+        client.query('INSERT INTO "entities"' +
+            ' (name,pseudonym,work,release,source_link)' +
+            ' VALUES' +
+            ' ($1,$2,$3,$4,$5)', [jsonContent[i].name, jsonContent[i].pseudonym, jsonContent[i].work, jsonContent[i].release, jsonContent[i].source_link],
+            function(err, res) {
+                if (err) {
+                    console.log(err);
+                }
+            }
+        );
+        i = i + 1;
+        if (i >= jsonContent.length) {
+            done();
         }
-	while(i<jsonContent.length){
-		var work=[],release=[],pseudonym=[];
-		client.query('INSERT INTO "entities"'
-				+ ' (name,pseudonym,work,release,source_link)'
-				+ ' VALUES'
-				+ ' ($1,$2,$3,$4,$5)'
-				,[jsonContent[i].name,jsonContent[i].pseudonym,jsonContent[i].work,jsonContent[i].release,jsonContent[i].source_link]
-				,function(err,res){
-					if(err){ 		
-						console.log(err);				
-					}
-				}
-			);
-		i=i+1;
-		if(i>=jsonContent.length){
-			done();
-		}
-	}
+    }
 });
