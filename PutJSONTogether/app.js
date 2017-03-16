38cd9f8f12d8a73312c309e25159e398c01dbb90
Tim Henkelmann <ga68vah@mytum.de>
2017-03-16 14:08:18 +0100

    Implemented improvement proposals: - add return to save on else block - initialize fields as null - introduce local reference to obj.table[i].aboutartist - restructured if-statement regarding dateOfBirth and dateOfDeath (line30++)

--- PutJSONTogether/app.js
+++ PutJSONTogether/app.js
@@ -6,63 +6,55 @@ var musicians = [];
 var newObj = {};
 
 function convertToInformation(content) {
-    fs.readFile(content, 'utf8', function readFileCallback(err, data){
-        if (err){
+    fs.readFile(content, 'utf8', function readFileCallback(err, data) {
+        if (err) {
             console.log(err);
-        } else {
-            obj = JSON.parse(data); //now it an object
-            arrayofObj = obj.table.length;
-
-            for(i = 0; i < obj.table.length; i++) {
-                newObj = {};
-                if (obj.table[i].hasOwnProperty("aboutartist")) {
-                    newObj.name = obj.table[i].aboutartist.name;
-                    newObj.nationality = null;
-
-                    if (obj.table[i].aboutartist.hasOwnProperty("life-span")) {
-                        if(obj.table[i].aboutartist["life-span"].hasOwnProperty("begin")) {
-                            if (obj.table[i].aboutartist["life-span"].begin != null) {
-                                newObj.dateOfBirth = obj.table[i].aboutartist["life-span"].begin;
-                            } else { newObj.dateOfBirth = null; } 
-                        } else { newObj.dateOfBirth = null; }
-                    } else { newObj.dateOfBirth = null; }
-
-                    if (obj.table[i].aboutartist.hasOwnProperty("life-span")) {
-                        if(obj.table[i].aboutartist["life-span"].hasOwnProperty("end")) {
-                            if (obj.table[i].aboutartist["life-span"].end != null) {
-                                newObj.dateOfDeath = obj.table[i].aboutartist["life-span"].end;
-                            } else { newObj.dateOfDeath = null; } 
-                        } else { newObj.dateOfDeath = null; }
-                    } else { newObj.dateOfDeath = null; }
-
-                    if(obj.table[i].aboutartist.hasOwnProperty("area")) {
-                        if (obj.table[i].aboutartist.area != null) {
-                            newObj.placeOfBirth = obj.table[i].aboutartist.area.name;
-                        } else { newObj.placeOfBirth = null; }
-                    } else { newObj.placeOfBirth = null; }
-
-                    if(obj.table[i].aboutartist.hasOwnProperty("end_area")) {
-                        if (obj.table[i].aboutartist["end_area"] != null) {
-                            newObj.placeOfDeath = obj.table[i].aboutartist["end_area"].name;
-                        } else { newObj.placeOfDeath = null; }
-                    } else { newObj.placeOfDeath = null; }
-
-
-                    newObj.instrument = null;
-                    newObj.psuedonym = null;
-                    newObj.work = null;
-                    newObj.release = null;
-                    newObj.tag = null;
-                    newObj.source_link = "https://musicbrainz.org/artist/"+obj.table[i].artistId;    
-
-                    musicians.push(newObj);
-                    
-                }
-                
-            }
-
-            return musicians;
+            return;
         }
+
+        obj = JSON.parse(data); //now it an object
+        arrayofObj = obj.table.length;
+
+        for (i = 0; i < obj.table.length; i++) {
+            newObj = {};
+            if (obj.table[i].hasOwnProperty("aboutartist")) {
+                let aboutArtist = obj.table[i].aboutartist;
+                newObj.name = aboutArtist.name;
+                newObj.nationality = null;
+
+                newObj.dateOfBirth = null;
+                newObj.dateOfDeath = null;
+                newObj.placeOfBirth = null;
+                newObj.placeOfDeath = null;
+
+                if (aboutArtist.hasOwnProperty("life-span")) {
+                    if (aboutArtist["life-span"].hasOwnProperty("begin") && aboutArtist["life-span"].begin != null) {
+                        newObj.dateOfBirth = aboutArtist["life-span"].begin;
+                    }
+                    if (aboutArtist["life-span"].hasOwnProperty("end") && aboutArtist["life-span"].end != null) {
+                        newObj.dateOfBirth = aboutArtist["life-span"].end;
+                    }
+                }
+
+                if (aboutArtist.hasOwnProperty("area") && aboutArtist.area != null) {
+                    newObj.placeOfBirth = aboutArtist.area.name;
+                }
+
+                if (aboutArtist.hasOwnProperty("end_area") && aboutArtist["end_area"] != null) {
+                    newObj.placeOfDeath = aboutArtist["end_area"].name;
+                }
+
+                newObj.instrument = null;
+                newObj.psuedonym = null;
+                newObj.work = null;
+                newObj.release = null;
+                newObj.tag = null;
+                newObj.source_link = "https://musicbrainz.org/artist/" + obj.table[i].artistId;
+
+                musicians.push(newObj);
+            }
+        }
+        return musicians;
     });
 }
 
