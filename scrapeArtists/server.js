3ee94932ac8f70cdb287043576d2fd804b75226f
ga89mam <luk.navickas@gmail.com>
2017-03-16 14:32:37 +0100

    remove json files and fix issues with the app

--- scrapeArtists/server.js
+++ scrapeArtists/server.js
@@ -1,57 +1,86 @@
 var request = require("request");
-var fs = require('fs');
 
-var elemNum = 0;
+  var listOfIDs = getIDs();
+  var artistInfo = getArtistInfo(listOfIDs);
 
-var newObj = {
+
+function getArtistInfo(listOfIDs) {
+
+    arrayofObj = listOfIDs.table;
+
+    var elemNum = 0;
+    var newObj = {
+      table: []
+    };
+
+    var finalArray = arrayofObj.map(function (artistai) {
+      return artistai.artistId;
+    });
+
+    setInterval(function(){      
+
+      var url = "http://musicbrainz.org/ws/2/artist/"+finalArray[elemNum]+"?&fmt=json";
+
+      if (elemNum == finalArray.length) {
+          return newObj; // return information about the artist after finished iterating
+      }
+
+      request({
+          url: url,
+          headers: {
+          'User-Agent': 'ClassicalApp/1.4.0'
+          },
+          json: true
+      }, function (error, response, body) {
+
+          var json = body;
+          newObj.table.push({artistId: finalArray[elemNum], aboutartist: json});
+          elemNum = elemNum + 1;
+                
+      });
+
+    }, 1800)
+}
+
+
+  function getIDs() {
+  var date = 1620
+  var obj = {
     table: []
-};
+  };
 
-fs.readFile('IDArtistsRecordingsFinal2.json', 'utf8', function readFileCallback(err, data) {
-    if (err) {
-        console.log(err);
-    } else {
+  setInterval(function(){
 
-        obj = JSON.parse(data); //now it an object
-        arrayofObj = obj.table;
-
-        var finalArray = arrayofObj.map(function (artistai) {
-            return artistai.artistId;
-        });
-
-        setInterval(function () {
-
-            var url = "http://musicbrainz.org/ws/2/artist/" + finalArray[elemNum] + "?&fmt=json";
-
-            if (elemNum == finalArray.length) {
-                json2 = JSON.stringify(newObj); //convert it back to json
-                fs.writeFile('artistsFromRecordings.json', json2, 'utf8', function writeFileCallback(err, data) {
-                    process.exit();
-                }); // write it back
-
-            }
-
-            console.log("requesting: " + url);
-
-
-            request({
-                url: url,
-                headers: {
-                    'User-Agent': 'ClassicalApp/1.4.0 '
-                },
-                json: true
-            }, function (error, response, body) {
-                console.log("adding " + body.name);
-                var json = body;
-                newObj.table.push({artistId: finalArray[elemNum], aboutartist: json});
-                elemNum = elemNum + 1;
-
-            });
-
-        }, 1800)
-
+    if(date == 1821) {
+      return obj;
     }
-});
 
+    var url = "http://musicbrainz.org/ws/2/artist/?query=begin:"+date+"&fmt=json";
 
+    request({
+        url: url,
+        headers: {
+        'User-Agent': 'ClassicalMusic/1.5.0 '
+        },
+        json: true
+    }, function (error, response, body) {
 
+          var json = body;
+          var counter = json.count;
+
+          if (counter > 0) {
+
+            for (var z = 0; z < counter; z++) {
+              artist = json.artists[z];    
+              obj.table.push({id: artist.id});
+            }  
+          } 
+
+          date = date + 1;               
+              
+    });
+      
+  }, 2000)
+  }
+
+        
