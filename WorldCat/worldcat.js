9cdff680e02b4e879d69b3349dee38b00602ce04
Angelin Rashmi Antony Rajan <Rashmi@Angelins-MacBook-Air.local>
2017-03-20 17:29:15 +0100

    revised code according to the comments

--- WorldCat/worldcat.js
+++ WorldCat/worldcat.js
@@ -1,96 +1,82 @@
 var fs = require('fs');
-var xhr = require('node-xhr');
+var request = require('request');
 var ceil = require('math-ceil');
 var numeral = require('numeral');
+var cheerio = require('cheerio');
 var page = 1,
     total_links = 1000000,
     j = 1,
     total_records, i = 0,
     n = 10,
     arrayList;
-var recording = [],
-    artist = [],
-    publisher = [],
-    source_link = [];
+var source_link = [],
+    musicians = [],
+    recording = [],
+    artist = [];
 var url = "https://www.worldcat.org/search?q=dt%3Asco&fq=&dblist=638&qt=page_number_link&start=" + page;
 console.log(url);
-console.log('request started!');
-var obj = {
-    table: []
-};
 var refreshIntervalId = setInterval(fname, 2000);
 
 function fname() {
-    xhr.post({
-        url: url,
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        params: {
-            // ...
-        },
-        body: {
-            // ...
-        },
-    }, function(err, res) {
-        if (err) {
-            console.log(err.message);
-            return;
-        }
-        var cheerio = require('cheerio'),
-            $ = cheerio.load(res.body);
+    request(url, function(error, response, body) {
+            $ = cheerio.load(body);
+            // Finds total records
+            if (page == 1) {
+                total_records = $('.resultsinfo strong').text();
+                $('.resultsinfo strong').each(function() {
+                    if (i == 1) {
+                        total_records = $(this).text();
+                    }
+                    i = i + 1;
+                });
+                console.log(total_records);
+                total_links = ceil((numeral(total_records)).value() / 10);
+                console.log(total_links);
+            }
 
-        // Finds total records
-        if (page == 1) {
-            console.log('rashmi');
-            total_records = $('.resultsinfo strong').text();
-            $('.resultsinfo strong').each(function() {
-                if (i == 1) {
-                    total_records = $(this).text();
+            $('td .name').each(function() {
+                recording.push(replaceTabNewLine($(this).text()));
+            });
+            $('td .author').each(function() {
+                artist.push(replaceTabNewLine($(this).text()));
+            });
+
+            page = page + 10;
+            j = j + 1;
+            if (j > total_links) {
+            k = 0;
+            while (k < 10) {
+                var release = [],
+                    name = [];
+                name = recording;
+                release = artist[k].split(";");
+                release[0] = release[0].substr(3, release[0].length);
+                console.log(name[k]);
+                console.log(release);
+                for (var index = 0; index < release.length; index++) {
+                    release[index] = release[index].trim();
+                    if (release[index] == "")
+                        release = release.splice(index, 1);
                 }
-                i = i + 1;
-            });
-            console.log(total_records);
-            total_links = ceil((numeral(total_records)).value() / 10);
-            console.log(total_links);
-        }
-
-        $('td .name').each(function() {
-            var temp = ($(this).text()).replace(/[\n\r]+/g, '');
-            temp = temp.replace(/\s{2,10}/g, '');
-            recording.push(temp);
-            source_link.push(url);
-        });
-        $('td .author').each(function() {
-            var temp = ($(this).text()).replace(/[\n\r]+/g, '');
-            temp = temp.replace(/\s{2,10}/g, '');
-            artist.push($(this).text());
-        });
-        $('td .itemPublisher').each(function() {
-            var temp = ($(this).text()).replace(/[\n\r]+/g, '');
-            temp = temp.replace(/\s{2,10}/g, '');
-            publisher.push($(this).text());
-        });
-        k = 0;
-        while (k < 10) {
-            obj.table.push({
-                recording_number: page + k,
-                recording: recording[k],
-                artist: artist[k],
-                publisher: publisher[k],
-                source: source_link[k]
-            });
-            k = k + 1;
-        }
-
-        page = page + 10;
-        j = j + 1;
-        if (j > total_links) {
-            json2 = JSON.stringify(obj); //convert it back to json
-            fs.writeFile('worldcat.json', json2, 'utf8'); // write it back
+                console.log(release);
+                musicians.push({
+                    release: release,
+                    name: name[k],
+                    source_link: source_link
+                });
+                k = k + 1;
+            }
+            var json2 = JSON.stringify(musicians); //convert it back to json
+            fs.writeFileSync('worldcat.json', json2, 'utf8'); // write it back
             clearInterval(refreshIntervalId);
+            process.exit();
         }
-        url = "https://www.worldcat.org/search?q=dt%3Asco&fq=&dblist=638&qt=page_number_link&start=" + page;
-        console.log(url);
+        url = "https://www.worldcat.org/search?q=dt%3Asco&fq=&dblist=638&qt=page_number_link&start=" + page; console.log(url);
     });
+}
+
+function replaceTabNewLine(str) {
+    str = str.replace(/[\n\r]+/g, '');
+    str = str.replace(/\s{2,10}/g, '');
+    return str;
 }
