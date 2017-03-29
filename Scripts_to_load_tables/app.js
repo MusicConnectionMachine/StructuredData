70cab38386112dd9e8bc531ef2f78b313c88b387
TimH <ga68vah@mytum.de>
2017-03-30 00:17:49 +0200

    removed those scripts as this will be handled in the CLI

--- Scripts_to_load_tables/app.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-var Sequelize = require('sequelize');
-var config = require('./config/postgresConfig.json');
-
-require(path.join(__dirname, "index1.js")).connect(function (context) {
-    require(path.join(__dirname, "load_artists.js")).load_artists(context);
-
-});
