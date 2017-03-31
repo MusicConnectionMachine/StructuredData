1e0932624227dcb28ead21b93b29888e7e46d9e7
ShilpaGhanashyamGore <ga89top@mytum.de>
2017-03-31 18:23:28 +0200

    some changes

--- dbpedia/dbpedia_Classical_musicians_by_instrument.js
+++ dbpedia/dbpedia_Classical_musicians_by_instrument.js
@@ -196,10 +196,6 @@ function fetch3(value2) {
 
         if (nationality.length == 0)
             nationality = null;
-        if (placeOfBirth.length == 0)
-            placeOfBirth = null;
-        if (placeOfDeath.length == 0)
-            placeOfDeath = null;
         if (wiki_link.length == 0)
             wiki_link = null;
         if (wiki_pageid.length == 0)
