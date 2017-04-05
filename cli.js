8b7a46716edae4f25e4a6bb256dbab08c6fe94fa
TimH <ga68vah@mytum.de>
2017-04-05 22:39:50 +0200

    enable threading for scraping scripts

--- cli.js
+++ cli.js
@@ -1,186 +1,191 @@
-/*
- to setup your own docker postgres db, install docker
- then in cmd/terminal, type:
- docker run -it --name my-postgres -p 5432:5432 postgres
-
- This will run your postgres db locally.
- With "docker ps -a" you can see all of your containers.
- Your my-postgres container should already be running. you can later stop and start it with "docker stop my-postgres" and
- "docker start my-postgres"
-
- Next, open pgadmin, right-click on servers on the left side, click on properties (and define a name if you haven't already).
- There in the second ribbon, set hostname as "localhost" and click save. You should now be connected to your postgres db.
-
- Now, you can execute this script. Navigate to this folder, in my case:
- cd git-repo
- Then execute it:
- node cli.js -s test
- This will add the scripts in the /testscripts folder and execute them. The dbpedia_musicians_nationality is changed such
- that it currently only fetches Frank Zappa and writes it to the output file.
- If everything worked correctly, CONGRATULATIONS! :D This entry should now be added to the db. You can check the artists
- table in pgadmin Servers->PSQL->Databases->postgres->schemas->public->tables->artists->
- rightclick and select "View Data ->View all data". Easy, right!?
- (If the script was successful but you cannot see any entries, you might need to restart pgadmin)
-
-
-
-
- THE NEXT STEPS TO IMPROVE THE SCRIPT:
-
-
- One would need to add a process.send to every script once its output file is written (see /testscripts/dbpedia_musicians_nationality.js)
-
-
- the "fs.readFile" in the populateDB function currently just gets the json file "testscripts/dbpedia_test.json". This would need to be solved
- dynamically. Either find all .json files in subfolders OR save all json outputfiles in one folder and then access them (second solution is
- probably better)
-
-
-
- */
 const commander = require('commander');
-const cp = require('child_process');
 const path = require('path');
 const fs = require('fs');
-//const path      = require('path');
-/*const parser    = require(path.resolve(__dirname,'parser'));
- const dataGridRenderer    = require(path.resolve(__dirname,'dataGridRenderer'));*/
-
-commander
-    .option('-s, --script [script]', 'Define the scripts that should be executed', /^(dbpedia|worldcat|musicbrainz|allmusic|test)$/i)
-    .option('-p, --postgres [postgres] ', 'Set the connection string to connect to the postgres db.')
-    .parse(process.argv);
-
-const script = commander.script || process.env.s;
-
-const postgresCS = commander.postgres || process.env.p;
-
+const cluster = require('cluster');
 var scriptsArray = [];
-if (script == "dbpedia") {
-    console.log("Adding dbpedia scripts");
-    scriptsArray.push("./dbpedia/dbpedia_Classical_musicians_by_century.js",
-        "./dbpedia/dbpedia_Classical_musicians_by_instrument.js",
-        "./dbpedia/dbpedia_Classical_musicians_by_instrument_and_nationality.js",
-        './dbpedia/dbpedia_Classical_musicians_by_nationality.js',
-        "./dbpedia/dbPedia_Composers.js"
-    );
-}
-if (script == "worldcat") {
-    console.log("Adding worldcat scripts");
-    scriptsArray.push("./worldcat/worldcat.js"
-    );
-}
-if (script == "musicbrainz") {
-    console.log("Adding musicbrainz scripts");
-    scriptsArray.push("./musicbrainz/scrapeArtists/server.js",
-        "./musicbrainz/scrapeRecordings/server.js",
-        "./musicbrainz/scrapeReleases/server.js",
-        "./musicbrainz/scrapeWorks/server.js",
-        "./musicbrainz/scrapeIDArtists/server.js",
-        "./musicbrainz/RecordingsArtistsID/app.js",
-        "./musicbrainz/DataForSequelizeAPI/app2.js",
-        "./musicbrainz/PutJSONTogether/app.js"
-    );
-}
-if (script == "allmusic") {
-    console.log("Adding almusic scripts");
-    scriptsArray.push("./allmusic/allMusicScript.js"
-    );
-}
-if (script == "test") {
-    console.log("Adding test scripts");
-    scriptsArray.push("./testscripts/test.js",
-        "./testscripts/test2.js",
-        "./testscripts/test3.js",
-        "./testscripts/dbpedia_musicians_nationality.js"
-    );
-}
-var arrayLength = scriptsArray.length;
-var cpCounter = 0;
-for (var i = 0; i < arrayLength; i++) {
-    var scriptToExecute = scriptsArray[i];
-    //this will create a child process for every script
-    const n = cp.fork(scriptToExecute);
+var script, postgresCS;
+if (cluster.isMaster) {
 
-    //once the script is done, it should send a message back to the main process
-    n.on('message', function (m) {
-        cpCounter++;
-        console.log('child process finished:', m);
-        //once all scripts are executed, continue with populating the db
-        if (cpCounter == arrayLength) {
+    commander
+        .option('-s, --script [script]', 'Define the scripts that should be executed', /^(dbpedia|worldcat|musicbrainz|allmusic|test)$/i)
+        .option('-d, --database [database] ', 'Set the connection string to connect to the database.')
+        .option('-t, --threads [threads] ', 'Set the amount of worker threads that should be spawned.')
+        .parse(process.argv);
+
+    script = commander.script || process.env.s;
+
+    postgresCS = commander.database || process.env.d;
+
+
+    if (script == "dbpedia") {
+        console.log("Adding dbpedia scripts");
+        scriptsArray.push("./dbpedia/dbpedia_Classical_musicians_by_century.js",
+            "./dbpedia/dbpedia_Classical_musicians_by_instrument.js",
+            "./dbpedia/dbpedia_Classical_musicians_by_instrument_and_nationality.js",
+            './dbpedia/dbpedia_Classical_musicians_by_nationality.js',
+            "./dbpedia/dbPedia_Composers.js"
+        );
+
+    }
+    if (script == "worldcat") {
+        console.log("Adding worldcat scripts");
+        scriptsArray.push("./worldcat/worldcat.js"
+        );
+    }
+    if (script == "musicbrainz") {
+        console.log("Adding musicbrainz scripts");
+        scriptsArray.push("./musicbrainz/scrapeArtists/server.js",
+            "./musicbrainz/scrapeRecordings/server.js",
+            "./musicbrainz/scrapeReleases/server.js",
+            "./musicbrainz/scrapeWorks/server.js",
+            "./musicbrainz/scrapeIDArtists/server.js",
+            "./musicbrainz/RecordingsArtistsID/app.js",
+            "./musicbrainz/DataForSequelizeAPI/app2.js",
+            "./musicbrainz/PutJSONTogether/app.js"
+        );
+    }
+    if (script == "allmusic") {
+        console.log("Adding almusic scripts");
+        scriptsArray.push("./allmusic/allMusicScript.js"
+        );
+    }
+    if (script == "test") {
+        console.log("Adding test scripts");
+        scriptsArray.push("./testscripts/test.js",
+            "./testscripts/test2.js",
+            "./testscripts/test3.js",
+            "./testscripts/dbpedia_musicians_nationality.js"
+        );
+    }
+
+
+    var cpus = require('os').cpus().length;
+    var numWorkers;
+
+    if (commander.threads) {
+        if (commander.threads > cpus) {
+            console.log("The number of your specified threads exceeds available cpus, reducing it to " + cpus);
+            numWorkers = cpus;
+        }
+        else {
+            console.log("Setting number of threads to " + commander.threads);
+            numWorkers = commander.threads;
+        }
+    }
+    else {
+        console.log("Setting number of threads to maximum of " + cpus);
+        numWorkers = cpus;
+    }
+
+    var scrapingcounter = scriptsArray.length;
+    console.log('Master cluster setting up ' + numWorkers + ' workers...');
+    for (var i = 0; i < numWorkers; i++) {
+        var worker = cluster.fork();
+        var scriptToExecute = scriptsArray.pop();
+        if (scriptToExecute) {
+            //let worker execute the scrape script
+            worker.send(scriptToExecute);
+        }
+
+    }
+
+    cluster.on('exit', function (worker, code, signal) {
+        console.log('Worker ' + worker.process.pid + ' died with code: ' + code + ', and signal: ' + signal);
+        var scriptToExecute = scriptsArray.pop();
+        if (scriptToExecute) {
+            // if a worker dies and there are still remaining scripts, spawn a new worker and let him execute it
+            var worker = cluster.fork();
+            worker.send(scriptToExecute);
+        }
+    });
+
+
+    cluster.on('message', function (m) {
+        scrapingcounter--;
+        if (scrapingcounter == 0) {
+            //once all scraping scripts finished, populate the db
+            console.log("all done");
             populateDB();
         }
-    })
-    ;
+    });
+}
+else {
+    process.on('message', function (scriptToExecute) {
+        console.log("Worker is executing " + scriptToExecute);
+        var script = require(scriptToExecute);
+        script(function () {
+            console.log(scriptToExecute + " finished successfully");
+            process.send("done");
+            process.exit();
+        })
+
+    });
 
 }
-;
 
 function populateDB() {
     console.log("Starting to populate db");
 
     require(path.join(__dirname, "api", "database.js")).connect(postgresCS, function (context) {
 
-        const api = require("./loadModules.js")(context, function () {
-            context.sequelize.sync({force: true}).then(function () {
+        context.sequelize.sync({force: true}).then(function () {
 
-                // bulk create all artists
-                const artists = context.component('models').module('artists');
-                const artistspath = path.join(__dirname, "scrapedoutput", "artists")
-                fs.readdir(artistspath, (err, files) => {
-                    files.forEach(file => {
-                        //for each file, read  it and do bulk create
-                        fs.readFile(path.join(artistspath, file), function (err, data) {
-                            artists.bulkCreate(JSON.parse(data))
-                                .then(function () {
-                                    console.log("Created artist entries for " + file);
-                                }).catch(function (error) {
-                                console.log("error: " + error);
-                            })
+            // bulk create all artists
+            const artists = context.models.artists;
+            const artistspath = path.join(__dirname, "scrapedoutput", "artists")
+            fs.readdir(artistspath, (err, files) => {
+                files.forEach(file => {
+                    //for each file, read  it and do bulk create
+                    fs.readFile(path.join(artistspath, file), function (err, data) {
+                        artists.bulkCreate(JSON.parse(data))
+                            .then(function () {
+                                console.log("Created artist entries for " + file);
+                            }).catch(function (error) {
+                            console.log("error: " + error);
                         })
-                    });
+                    })
                 });
-
-                // bulk create all works
-                const works = context.component('models').module('works');
-                const workspath = path.join(__dirname, "scrapedoutput", "works")
-                fs.readdir(workspath, (err, files) => {
-                    files.forEach(file => {
-                        //for each file, read  it and do bulk create
-                        fs.readFile(path.join(workspath, file), function (err, data) {
-                            works.bulkCreate(JSON.parse(data))
-                                .then(function () {
-                                    console.log("Created work entries for " + file);
-                                }).catch(function (error) {
-                                console.log("error: " + error);
-                            })
-                        })
-                    });
-                });
-
-                // bulk create all releases
-                const releases = context.component('models').module('releases');
-                const releasespath = path.join(__dirname, "scrapedoutput", "releases")
-                fs.readdir(releasespath, (err, files) => {
-                    files.forEach(file => {
-                        //for each file, read  it and do bulk create
-                        fs.readFile(path.join(releasespath, file), function (err, data) {
-                            releases.bulkCreate(JSON.parse(data))
-                                .then(function () {
-                                    console.log("Created release entries for " + file);
-                                }).catch(function (error) {
-                                console.log("error: " + error);
-                            })
-                        })
-                    });
-                });
-
-
-            }).catch(function (error) {
-                console.error("There was an error while syncronizing the tables between the application and the database.");
-                console.error(error);
-                process.exit(2);
             });
+
+            // bulk create all works
+            const works = context.models.works;
+            const workspath = path.join(__dirname, "scrapedoutput", "works")
+            fs.readdir(workspath, (err, files) => {
+                files.forEach(file => {
+                    //for each file, read  it and do bulk create
+                    fs.readFile(path.join(workspath, file), function (err, data) {
+                        works.bulkCreate(JSON.parse(data))
+                            .then(function () {
+                                console.log("Created work entries for " + file);
+                            }).catch(function (error) {
+                            console.log("error: " + error);
+                        })
+                    })
+                });
+            });
+
+            // bulk create all releases
+            const releases = context.models.releases;
+            const releasespath = path.join(__dirname, "scrapedoutput", "releases")
+            fs.readdir(releasespath, (err, files) => {
+                files.forEach(file => {
+                    //for each file, read  it and do bulk create
+                    fs.readFile(path.join(releasespath, file), function (err, data) {
+                        releases.bulkCreate(JSON.parse(data))
+                            .then(function () {
+                                console.log("Created release entries for " + file);
+                            }).catch(function (error) {
+                            console.log("error: " + error);
+                        })
+                    })
+                });
+            });
+
+
+        }).catch(function (error) {
+            console.error("There was an error while syncronizing the tables between the application and the database.");
+            console.error(error);
+            process.exit(2);
         });
 
 
--- dbpedia/dbpedia_Classical_musicians_by_instrument.js
+++ dbpedia/dbpedia_Classical_musicians_by_instrument.js
@@ -1,101 +1,104 @@
 /////////////////////EXTRACTS ALL CLASSICAL MUSICIANS BY INSTRUMENT///////////////
-var fs = require('fs');
-var sleep = require('system-sleep');
-var request = require("request");
-var cheerio = require('cheerio');
-var jsesc = require('jsesc');
-const replaceURLAndUnderscore = require('./helper/replaceURLAndUnderscore');
-var temp_list = [],
-    source_link = [],
-    musicians = [],
-    url = [];
-url.push("http://dbpedia.org/page/Category:Classical_musicians_by_instrument");
-console.log("---dbpedia_Classical_musicians_by_instrument.js started!---")
+module.exports = function (returnToMaster) {
+    var fs = require('fs');
+    var sleep = require('system-sleep');
+    var request = require("request");
+    var cheerio = require('cheerio');
+    var jsesc = require('jsesc');
+    const replaceURLAndUnderscore = require('./helper/replaceURLAndUnderscore');
+    var temp_list = [],
+        source_link = [],
+        musicians = [],
+        url = [];
+    url.push("http://dbpedia.org/page/Category:Classical_musicians_by_instrument");
+    console.log("---dbpedia_Classical_musicians_by_instrument.js started!---")
 
-url.forEach(function (value) {
-    scrapeURL(value);
-});
-
-function scrapeURL(link1) {
-    request(link1, function (error, response, body) {
-        if (error) {
-            console.log("dbpedia_Classical_musicians_by_instrument.js scrapeURL: " + error);
-            return
-        }
-
-        $ = cheerio.load(body);
-        $('a[rev="skos:broader"]').each(function (i, element) {
-            var a = $(this);
-            temp_list.push(a.attr('href'));
-        });
-
-        temp_list.forEach(function (value) {
-            scrapeCat(value);
-            sleep(2000);
-
-        });
-        source_link.forEach(function (value) {
-            scrapeArtist(value);
-            sleep(1000);
-        });
-        json2 = JSON.stringify(musicians); //convert it back to json
-        fs.writeFileSync('./scrapedoutput/artists/dbpedia_Classical_musicians_by_instruments.json', json2, 'utf8'); // write it back
-        process.send("done dbpedia_Classical_musicians_by_instrument.js");
-
-
+    url.forEach(function (value) {
+        scrapeURL(value);
     });
-}
+
+    function scrapeURL(link1) {
+        request(link1, function (error, response, body) {
+            if (error) {
+                console.log("dbpedia_Classical_musicians_by_instrument.js scrapeURL: " + error);
+                return
+            }
+
+            $ = cheerio.load(body);
+            $('a[rev="skos:broader"]').each(function (i, element) {
+                var a = $(this);
+                temp_list.push(a.attr('href'));
+            });
+
+            temp_list.forEach(function (value) {
+                scrapeCat(value);
+                sleep(2000);
+
+            });
+            source_link.forEach(function (value) {
+                scrapeArtist(value);
+                sleep(1000);
+            });
+            json2 = JSON.stringify(musicians); //convert it back to json
+            fs.writeFileSync('./scrapedoutput/artists/dbpedia_Classical_musicians_by_instruments.json', json2, 'utf8'); // write it back
+            returnToMaster();
 
 
-function scrapeCat(value2) {
-    request(value2, function (error, response, body) {
-        if (error) {
-            console.log("dbpedia_Classical_musicians_by_instrument.js scrapeCat: " + error);
-            return
-        }
-        $ = cheerio.load(body);
-        $('a[rev="dct:subject"]').each(function (i, element) {
-            var a = $(this);
-            source_link.push(a.attr('href'));
         });
-
-    });
-}
+    }
 
 
-function scrapeArtist(value2) {
-    request(jsesc(value2), function (error, response, body) {
+    function scrapeCat(value2) {
+        request(value2, function (error, response, body) {
+            if (error) {
+                console.log("dbpedia_Classical_musicians_by_instrument.js scrapeCat: " + error);
+                return
+            }
+            $ = cheerio.load(body);
+            $('a[rev="dct:subject"]').each(function (i, element) {
+                var a = $(this);
+                source_link.push(a.attr('href'));
+            });
 
-        if (error) {
-            console.log("dbpedia_Classical_musicians_by_instrument.js scrapeArtist: " + error);
-            return
-        }
-        $ = cheerio.load(body);
-        var split_name = (replaceURLAndUnderscore(value2)).split("(");
-        var nationality = $('span[property="dbo:nationality"]').text().trim();
-        if (nationality.length == 0)
-            nationality = null;
-
-        var scrapedbpediaProperties = require('./helper/scrapedbpediaProperties.js');
-
-        var scrapedData = scrapedbpediaProperties($);
-
-        musicians.push({
-            name: split_name[0].trim(),
-            artist_type: 'musician',
-            nationality: nationality,
-            source_link: value2,
-            dateOfBirth: scrapedData.dateOfBirth,
-            dateOfDeath: scrapedData.dateOfDeath,
-            placeOfBirth: scrapedData.placeOfBirth,
-            placeOfDeath: scrapedData.placeOfDeath,
-            instrument: scrapedData.instrument,
-            pseudonym: scrapedData.pseudonym,
-            work: scrapedData.work,
-            release: scrapedData.release,
-            tags: scrapedData.tags,
-            wiki_link: scrapedData.wiki_link,
-            wiki_pageid: scrapedData.wiki_pageid
         });
-    });
+    }
+
+
+    function scrapeArtist(value2) {
+        console.log("dbpedia_Classical_musicians_by_instrument.js scraping artist");
+        request(jsesc(value2), function (error, response, body) {
+
+            if (error) {
+                console.log("dbpedia_Classical_musicians_by_instrument.js scrapeArtist: " + error);
+                return
+            }
+            $ = cheerio.load(body);
+            var split_name = (replaceURLAndUnderscore(value2)).split("(");
+            var nationality = $('span[property="dbo:nationality"]').text().trim();
+            if (nationality.length == 0)
+                nationality = null;
+
+            var scrapedbpediaProperties = require('./helper/scrapedbpediaProperties.js');
+
+            var scrapedData = scrapedbpediaProperties($);
+
+            musicians.push({
+                name: split_name[0].trim(),
+                artist_type: 'musician',
+                nationality: nationality,
+                source_link: value2,
+                dateOfBirth: scrapedData.dateOfBirth,
+                dateOfDeath: scrapedData.dateOfDeath,
+                placeOfBirth: scrapedData.placeOfBirth,
+                placeOfDeath: scrapedData.placeOfDeath,
+                instrument: scrapedData.instrument,
+                pseudonym: scrapedData.pseudonym,
+                work: scrapedData.work,
+                release: scrapedData.release,
+                tags: scrapedData.tags,
+                wiki_link: scrapedData.wiki_link,
+                wiki_pageid: scrapedData.wiki_pageid
+            });
+        });
+    }
 }
