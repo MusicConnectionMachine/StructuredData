ef5dab2343674bae56787e6b4c045ef71c2aab96
TimH <ga68vah@mytum.de>
2017-04-07 18:32:43 +0200

    added attributes to releases

--- musicbrainz/MusicBrainzCLI/getReleases.js
+++ musicbrainz/MusicBrainzCLI/getReleases.js
@@ -1,74 +1,74 @@
 var request = require("request");
 var fs = require('fs');
-var intervalId2 = null;
 
-var newObj2 = {};
 var releasesAPI = [];
 
 module.exports = {
-  getReleases: function(finalArray) {
+    getReleases: function (finalArray, returnToServerJS) {
 
-    var elemNum = 0;
+        var elemNum = 0;
 
-    var newObj = {
-        table: []
-    };
+        var artists = [];
 
- intervalId2 = setInterval(function () {
-
-        if (elemNum == finalArray.length) {
-
-            clearInterval(intervalId2);
-
-            obj = newObj;
-
-            for(i = 0; i < obj.table.length; i++) {
-            var counter = obj.table[i].count;
-                for(j = 0; j < counter; j++) {
-                    newObj2 = {};
-                    if (obj.table[i].releases[j] != null) {
-                        newObj2.title = obj.table[i].releases[j].title;
-                        releasesAPI.push(newObj2);
-                    }     
-                }    
-            }
-
-            json2 = JSON.stringify(releasesAPI); //convert it back to json
-            fs.writeFile('./scrapedoutput/BrainzReleasesSequelize.json', json2, 'utf8', function writeFileCallback(err, data) {
-                process.send("done from Releases");
-                process.exit();
-            }); // write it back
+        var intervalId2 = setInterval(function () {
 
 
-        }
-
-        var url = "http://musicbrainz.org/ws/2/release/?query=arid:" + finalArray[elemNum] + "&fmt=json";
-
-        console.log(url);
-
-        console.log('request Release started!');
-
-        request({
-            url: url,
-            headers: {
-                'User-Agent': 'ClassicalMusicApp/1.2.0 '
-            },
-            json: true
-        }, function (error, response, body) {
-
-            var json = body;
-
-            newObj.table.push({artistId: finalArray[elemNum], count: json.count, releases: json.releases});
-            elemNum = elemNum + 1;
-
-        });
+            var url = "http://musicbrainz.org/ws/2/release/?query=arid:" + finalArray[elemNum] + "&fmt=json";
 
 
-    }, 1500)
+            console.log('request releases for artist ' + finalArray[elemNum]);
+
+            request({
+                url: url,
+                headers: {
+                    'User-Agent': 'ClassicalMusicApp/1.2.0 '
+                },
+                json: true
+            }, function (error, response, body) {
+
+                var json = body;
+                artists.push({artistId: finalArray[elemNum], count: json.count, releases: json.releases});
+                elemNum = elemNum + 1;
+                if (elemNum == finalArray.length) {
+
+                    clearInterval(intervalId2);
 
 
-  },
-       
+                    for (i = 0; i < artists.length; i++) {
+                        var counter = artists[i].count;
+                        for (j = 0; j < counter; j++) {
+                            var releaseObject = {};
+                            if (artists[i].releases[j] != null) {
+                                var currReleases = artists[i].releases[j]
+                                releaseObject.title = artists[i].releases[j].title;
+                                releaseObject.format = artists[i].releases[j].media[0].format;
+                                releaseObject.date = artists[i].releases[j].date;
+                                releaseObject.country = artists[i].releases[j]["label-info"][0].label.name;
+                                releaseObject.label = artists[i].releases[j].title;
+                                releasesAPI.push({
+                                    title: currReleases.title,
+                                    format: currReleases.media[0].format,
+                                    date: currReleases.date,
+                                    country: currReleases.country,
+                                    label: currReleases["label-info"][0].label.name,
+                                    artistId: artists[i].artistId
+                                });
+                            }
+                        }
+                    }
+                    json2 = JSON.stringify(releasesAPI); //convert it back to json
+                    fs.writeFile('./scrapedoutput/releases/BrainzReleasesSequelize.json', json2, 'utf8', function writeFileCallback(err, data) {
+                        returnToServerJS();
+                    }); // write it back
+                }
+
+            });
+
+        }, 1500)
+
+
+    },
+
 };
 
 
