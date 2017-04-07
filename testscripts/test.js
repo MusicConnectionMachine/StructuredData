dd0969733074ca5d47b6eb246dbab06232c5943b
TimH <ga68vah@mytum.de>
2017-04-07 10:36:55 +0200

    removed test, test2, test3

--- testscripts/test.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by TimH on 28-Mar-17.
- */
-console.log("test 1");
-process.send("done 1");
--- testscripts/test3.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by TimH on 28-Mar-17.
- */
-console.log("test 3");
-process.send("done 3");
